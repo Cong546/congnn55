1215f0269f066d0a8974ef40cdc72566c49c3dbd
Cong546 <nguyenninhcong1234@gmail.com>
2025-06-28 14:02:02 +0700

    .....

--- b1.js
+++ b1.js
@@ -1,33 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+let form = document.querySelector("form");
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDa0fVdtFDjxy_BtLm7X5ihsbpUILB1QTk",
-  authDomain: "cong12345-307d7.firebaseapp.com",
-  projectId: "cong12345-307d7",
-  storageBucket: "cong12345-307d7.firebasestorage.app",
-  messagingSenderId: "240405272298",
-  appId: "1:240405272298:web:bfd141f28c4a0a69be2993",
-  measurementId: "G-NZBHPVCBG2"
-};
+function login(e) {
+  e.preventDefault();
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let users = JSON.parse(localStorage.getItem("users"));
+  for (let i = 0; i < users.length; i++) {
+    if (username === users[i].username && password === users[i].password) {
+      alert("Login successful");
+      window.location.href = "trangchu.html";
+      return;
+    }
+  }
+  alert("Wrong username or password");
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-console.log(app);
-const auth = getAuth(app)
-const provider = new GoogleAuthProvider();
-let btn = document.getElementById('login')
-btn.addEventListener("click", function () {
-    signInWithPopup(auth, provider)
-    .then((result) => {
-        const user = result.user;
-        console.log(user);
-        alert("Login success");
-        window.location.href='./trangchu.html';
-    })
-})
+form.addEventListener("submit", login);
